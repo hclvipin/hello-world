26618f580f09e957c8c45dc489cb4c793e6e2e0c
hclvipin <vipin.r@hcl.com>
2016-06-15 13:53:40 +0530

    Test project
    Test project

---
+++ ZTestProject/src/com/hcl/test/TestHashMap.java
@@ -0,0 +1,38 @@
+package com.hcl.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < 15; i++) {
+			map.put("key" + i, new Integer(i + 1));
+		}
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		System.out.println("--------------------------------");
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		System.out.println("--------------------------------");
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		
+		String s1 = "vishal";
+		String s2 = new String(s1);
+		System.out.println(s2);
+		System.out.println(s1==s2);
+		
+		ArrayList<String> l = new ArrayList<>();
+		
+	}
+
+}
