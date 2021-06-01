d078a380cc853cc770774bed39b49695d13de8c5
Vipin Kumar Rajput <vipin.r@hcl.com>
2021-06-01 16:55:52 -0700

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,8 +4,24 @@ dbFeDeploySrcList=findFiles(excludes: '', glob: '${vars.deploySource}\\?_?eploy'
 for (ii = 0; ii < dbFeDeploySrcList.size(); ii++) {
 	if (dbFeDeploySrcList[ii].equalsIgnoreCase("fe_deploy")) {
 		vars.put("feDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
+		vars.put("feDeploySourceFolder", dbFeDeploySrcList[ii])
 	}
 	if (dbFeDeploySrcList[ii].equalsIgnoreCase("db_deploy")) {
 		vars.put("dbDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
+		vars.put("dbDeploySourceFolder", dbFeDeploySrcList[ii])
 	}
 }
+
+
+
+
+
+in fe zip > 
+
+glob: "${vars.feDeploySourceFolder}/**"
+
+	
+in db zip > 
+	
+glob: "${vars.dbDeploySourceFolder}/**"
+	
