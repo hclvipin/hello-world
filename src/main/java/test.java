99fbfbdadb8d4e252e9cb4e040667a33f181d387
Vipin Kumar Rajput <vipin.r@hcl.com>
2021-06-03 11:31:48 -0700

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,18 +1,27 @@
 
 dbFeDeploySrcList=findFiles(excludes: '', glob: '${vars.deploySource}\\?_?')
 
-for (ii = 0; ii < dbFeDeploySrcList.size(); ii++) {
-	if (dbFeDeploySrcList[ii].equalsIgnoreCase("fe_deploy")) {
-		vars.put("feDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
-		vars.put("feDeploySourceFolder", dbFeDeploySrcList[ii])
+// for (ii = 0; ii < dbFeDeploySrcList.size(); ii++) {
+// 	if (dbFeDeploySrcList[ii].equalsIgnoreCase("fe_deploy")) {
+// 		vars.put("feDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
+// 		vars.put("feDeploySourceFolder", dbFeDeploySrcList[ii])
+// 	}
+// 	if (dbFeDeploySrcList[ii].equalsIgnoreCase("db_deploy")) {
+// 		vars.put("dbDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
+// 		vars.put("dbDeploySourceFolder", dbFeDeploySrcList[ii])
+// 	}
+// }
+
+dbFeDeploySrcList.each { _file ->
+	if ("${_file.name}".equalsIgnoreCase("fe_deploy")) {
+		vars.put("feDeploySource", "$vars.deploySource\\" + "${_file.name}")
+		vars.put("feDeploySourceFolder", "${_file.name}")
 	}
-	if (dbFeDeploySrcList[ii].equalsIgnoreCase("db_deploy")) {
-		vars.put("dbDeploySource", "$vars.deploySource\\" + dbFeDeploySrcList[ii])
-		vars.put("dbDeploySourceFolder", dbFeDeploySrcList[ii])
+	if ("${_file.name}".equalsIgnoreCase("db_deploy")) {
+		vars.put("dbDeploySource", "$vars.deploySource\\" + "${_file.name}")
+		vars.put("dbDeploySourceFolder", "${_file.name}")
 	}
 }
-
-
 
 
 
